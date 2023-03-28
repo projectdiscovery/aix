31632b4df006fffb1a7de1eb4a7b70ff8f1275de
xm1k3 <xm1k3@protonmail.com>
2023-03-28 22:48:04 +0200

    simple fix on options in examples

--- examples/main.go
+++ examples/main.go
@@ -8,7 +8,20 @@ import (
 )
 
 func main() {
-	options := runner.ParseOptions()
+	options := runner.Options{
+		OpenaiApiKey:       "API-KEY",
+		Prompt:             "what is the capital of france?",
+		Gpt3:               true,
+		Gpt4:               false,
+		Update:             false,
+		DisableUpdateCheck: false,
+		Output:             "out.txt",
+		Jsonl:              false,
+		Verbose:            false,
+		Silent:             true,
+		NoColor:            false,
+		Version:            false,
+	}
 	aixRunner, err := runner.NewRunner(options)
 	if err != nil {
 		gologger.Fatal().Msgf("Could not create runner: %s\n", err)
